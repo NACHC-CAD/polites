3879e995d5a4747c7f3418d0289b051a124fd59f
greshje <greshje@gmail.com>
2024-05-03 07:47:57 -0400

    ! ! ! IT WORKS ! ! !

--- src/main/java/org/nachc/tools/politesforsqlserver/util/action/ExecutePolitesGoAction.java
+++ src/main/java/org/nachc/tools/politesforsqlserver/util/action/ExecutePolitesGoAction.java
@@ -1,9 +1,87 @@
 package org.nachc.tools.politesforsqlserver.util.action;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import org.nachc.tools.fhirtoomop.tools.build.impl.AddConstraints;
+import org.nachc.tools.fhirtoomop.tools.build.impl.BurnEverythingToTheGround;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateCdmSourceRecord;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateDatabase;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateDatabaseIndexes;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateDatabaseTables;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateDatabaseUser;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateFhirResoureTables;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateMappingTables;
+import org.nachc.tools.fhirtoomop.tools.build.impl.CreateSequencesForPrimaryKeys;
+import org.nachc.tools.fhirtoomop.tools.build.impl.LoadMappingTables;
+import org.nachc.tools.fhirtoomop.tools.build.impl.LoadTerminology;
+import org.nachc.tools.fhirtoomop.tools.build.impl.MoveRaceEthFiles;
+import org.nachc.tools.politesforsqlserver.util.connection.PolitesConnectionFactory;
+import org.yaorma.database.Database;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ExecutePolitesGoAction {
 
-	public static void exec() {
-		
+	public static void exec(ArrayList<String> sel, String databaseType, String cdmVersion) {
+		Connection conn = PolitesConnectionFactory.getBootstrapConnection();
+		try {
+			if (sel.contains("burnEverythingToTheGround")) {
+				// delete the existing instance
+				log.info("BURNING EVERYTHING TO THE GROUND");
+				BurnEverythingToTheGround.exec(conn);
+			}
+			if (sel.contains("createDatabase")) {
+				// create the new database
+				log.info("CREATING DATABASE");
+				log.info("Creating OMOP instance...");
+				CreateDatabase.exec(conn);
+			}
+			if (sel.contains("createDatabaseUsers")) {
+				// create the user
+				log.info("CREATING USER");
+				CreateDatabaseUser.exec(conn);
+			}
+			if (sel.contains("createTables")) {
+				// create the tables
+				log.info("CREATING TABLES");
+				CreateDatabaseTables.exec(conn);
+				CreateFhirResoureTables.exec(conn);
+				CreateMappingTables.exec(conn);
+			}
+			if (sel.contains("createCDMSourceRecord")) {
+				// create the cdm_source record (uses app.parameters values)
+				CreateCdmSourceRecord.exec(conn);
+				Database.commit(conn);
+			}
+			if (sel.contains("loadTerminology")) {
+				// move the race eth files
+				MoveRaceEthFiles raceFiles = new MoveRaceEthFiles();
+				raceFiles.exec();
+				// load the terminologies
+				log.info("LOADING TERMINOLOGY");
+				LoadMappingTables.exec(raceFiles.getSqlFile(), conn);
+				LoadTerminology.exec(conn);
+			}
+			if (sel.contains("createSequencesForPrimaryKeys")) {
+				// create the sequences
+				log.info("CREATING SEQUENCES");
+				CreateSequencesForPrimaryKeys.exec(conn);
+			}
+			if (sel.contains("createIndexes")) {
+				// create the indexes and add constraints
+				log.info("CREATING CONSTRAINTS");
+				CreateDatabaseIndexes.exec(conn);
+			}
+			if (sel.contains("addConstraints")) {
+				// create the indexes and add constraints
+				log.info("ADDING CONSTRAINTS");
+				AddConstraints.exec();
+			}
+		} finally {
+			Database.close(conn);
+		}
 	}
-	
+
 }
--- src/main/java/org/nachc/tools/politesforsqlserver/util/gui/PolitesGui.java
+++ src/main/java/org/nachc/tools/politesforsqlserver/util/gui/PolitesGui.java
@@ -1,192 +1,239 @@
 package org.nachc.tools.politesforsqlserver.util.gui;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import org.nachc.tools.politesforsqlserver.util.action.ExecutePolitesGoAction;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PolitesGui extends JFrame {
-	
-    private JCheckBox selectAllCheckbox;
-    private JCheckBox burnEverythingToTheGround;
-    private JCheckBox createDatabase;
-    private JCheckBox createDatabaseUsers;
-    private JCheckBox createTables;
-    private JCheckBox createCDMSourceRecord;
-    private JCheckBox loadTerminology;
-    private JCheckBox createSequencesForPrimaryKeys;
-    private JCheckBox createIndexes;
-    private JCheckBox addConstraints;
-    private JComboBox<String> databaseType, cdmVersion;
-    private JButton goButton;
 
-    public PolitesGui() {
+	private JCheckBox selectAllCheckbox;
+	private JCheckBox burnEverythingToTheGround;
+	private JCheckBox createDatabase;
+	private JCheckBox createDatabaseUsers;
+	private JCheckBox createTables;
+	private JCheckBox createCDMSourceRecord;
+	private JCheckBox loadTerminology;
+	private JCheckBox createSequencesForPrimaryKeys;
+	private JCheckBox createIndexes;
+	private JCheckBox addConstraints;
+	private JComboBox<String> databaseType, cdmVersion;
+	private JButton goButton;
 
-    	super("Polites");
-        setLayout(new BorderLayout());
+	public PolitesGui() {
 
-        //
-        // checkboxes
-        //
-        
-        burnEverythingToTheGround = new JCheckBox("Burn Everything to the Ground");
-        createDatabase = new JCheckBox("Create Database");
-        createDatabaseUsers = new JCheckBox("Create Database Users");
-        createTables = new JCheckBox("Create Tables");
-        createCDMSourceRecord = new JCheckBox("Create CDM Source Record");
-        loadTerminology = new JCheckBox("Load Terminology");
-        createSequencesForPrimaryKeys = new JCheckBox("Create Sequences for Primary Keys");
-        createIndexes = new JCheckBox("Create Indexes");
-        addConstraints = new JCheckBox("Add Constraints");
+		super("Polites");
+		setLayout(new BorderLayout());
 
-        JPanel checkboxPanel = new JPanel();
-        checkboxPanel.setLayout(new BoxLayout(checkboxPanel, BoxLayout.Y_AXIS));
+		//
+		// checkboxes
+		//
 
-        // Special checkbox for Select/Deselect All
-        selectAllCheckbox = new JCheckBox("Select/Deselect All");
-        selectAllCheckbox.setForeground(Color.GRAY);
-        selectAllCheckbox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                boolean selected = selectAllCheckbox.isSelected();
-                burnEverythingToTheGround.setSelected(selected);
-                createDatabase.setSelected(selected);
-                createDatabaseUsers.setSelected(selected);
-                createTables.setSelected(selected);
-                createCDMSourceRecord.setSelected(selected);
-                loadTerminology.setSelected(selected);
-                createSequencesForPrimaryKeys.setSelected(selected);
-                createIndexes.setSelected(selected);
-                addConstraints.setSelected(selected);
-            }
-        });
-        checkboxPanel.add(selectAllCheckbox);
+		burnEverythingToTheGround = new JCheckBox("Burn Everything to the Ground");
+		createDatabase = new JCheckBox("Create Database");
+		createDatabaseUsers = new JCheckBox("Create Database Users");
+		createTables = new JCheckBox("Create Tables");
+		createCDMSourceRecord = new JCheckBox("Create CDM Source Record");
+		loadTerminology = new JCheckBox("Load Terminology");
+		createSequencesForPrimaryKeys = new JCheckBox("Create Sequences for Primary Keys");
+		createIndexes = new JCheckBox("Create Indexes");
+		addConstraints = new JCheckBox("Add Constraints");
 
-        // Add individual checkboxes to the panel
-        checkboxPanel.add(burnEverythingToTheGround);
-        checkboxPanel.add(createDatabase);
-        checkboxPanel.add(createDatabaseUsers);
-        checkboxPanel.add(createTables);
-        checkboxPanel.add(createCDMSourceRecord);
-        checkboxPanel.add(loadTerminology);
-        checkboxPanel.add(createSequencesForPrimaryKeys);
-        checkboxPanel.add(createIndexes);
-        checkboxPanel.add(addConstraints);
+		JPanel checkboxPanel = new JPanel();
+		checkboxPanel.setLayout(new BoxLayout(checkboxPanel, BoxLayout.Y_AXIS));
 
-        JScrollPane scrollPane = new JScrollPane(checkboxPanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+		// Special checkbox for Select/Deselect All
+		selectAllCheckbox = new JCheckBox("Select/Deselect All");
+		selectAllCheckbox.setForeground(Color.GRAY);
+		selectAllCheckbox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean selected = selectAllCheckbox.isSelected();
+				burnEverythingToTheGround.setSelected(selected);
+				createDatabase.setSelected(selected);
+				createDatabaseUsers.setSelected(selected);
+				createTables.setSelected(selected);
+				createCDMSourceRecord.setSelected(selected);
+				loadTerminology.setSelected(selected);
+				createSequencesForPrimaryKeys.setSelected(selected);
+				createIndexes.setSelected(selected);
+				addConstraints.setSelected(selected);
+			}
+		});
+		checkboxPanel.add(selectAllCheckbox);
 
-        //
-        // combo boxes and button panel
-        //
-        
-        JPanel controlPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        databaseType = new JComboBox<>(new String[]{"sqlServer", "databricks"});
-        databaseType.setRenderer(new DefaultListCellRenderer() {
-            @Override
-            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-                super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                if ("sqlServer".equals(value)) {
-                    setText("SQL Server");
-                } else if ("databricks".equals(value)) {
-                    setText("Databricks");
-                }
-                return this;
-            }
-        });
-        cdmVersion = new JComboBox<>(new String[]{"cdm53"});
-        cdmVersion.setRenderer(new DefaultListCellRenderer() {
-            @Override
-            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-                super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                setText("CDM 5.3");
-                return this;
-            }
-        });
-        controlPanel.add(databaseType);
-        controlPanel.add(cdmVersion);
+		// Add individual checkboxes to the panel
+		checkboxPanel.add(burnEverythingToTheGround);
+		checkboxPanel.add(createDatabase);
+		checkboxPanel.add(createDatabaseUsers);
+		checkboxPanel.add(createTables);
+		checkboxPanel.add(createCDMSourceRecord);
+		checkboxPanel.add(loadTerminology);
+		checkboxPanel.add(createSequencesForPrimaryKeys);
+		checkboxPanel.add(createIndexes);
+		checkboxPanel.add(addConstraints);
 
-        goButton = new JButton("Go");
-        goButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                handleGoButton();
-            }
-        });
-        controlPanel.add(goButton);
+		JScrollPane scrollPane = new JScrollPane(checkboxPanel);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBorder(BorderFactory.createEmptyBorder());
 
-        //
-        // final layout
-        //
-        
-        add(scrollPane, BorderLayout.CENTER);
-        add(controlPanel, BorderLayout.SOUTH);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(650, 325);
-        setLocationRelativeTo(null);
-        setVisible(true);
-    }
+		//
+		// combo boxes and button panel
+		//
 
-    //
-    // handler for go button
-    //
-    
-    private void handleGoButton() {
-        // Build the confirmation message
-        StringBuilder confirmationMessage = new StringBuilder("You have selected:\n");
-        if (burnEverythingToTheGround.isSelected()) confirmationMessage.append("- Burn Everything to the Ground\n");
-        if (createDatabase.isSelected()) confirmationMessage.append("- Create Database\n");
-        if (createDatabaseUsers.isSelected()) confirmationMessage.append("- Create Database Users\n");
-        if (createTables.isSelected()) confirmationMessage.append("- Create Tables\n");
-        if (createCDMSourceRecord.isSelected()) confirmationMessage.append("- Create CDM Source Record\n");
-        if (loadTerminology.isSelected()) confirmationMessage.append("- Load Terminology\n");
-        if (createSequencesForPrimaryKeys.isSelected()) confirmationMessage.append("- Create Sequences for Primary Keys\n");
-        if (createIndexes.isSelected()) confirmationMessage.append("- Create Indexes\n");
-        if (addConstraints.isSelected()) confirmationMessage.append("- Add Constraints\n");
+		JPanel controlPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		databaseType = new JComboBox<>(new String[] { "sqlServer" });
+		databaseType.setRenderer(new DefaultListCellRenderer() {
+			@Override
+			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+				super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				if ("sqlServer".equals(value)) {
+					setText("SQL Server");
+				}
+				return this;
+			}
+		});
 
-        confirmationMessage.append("Database Type: ").append(databaseType.getSelectedItem()).append("\n");
-        confirmationMessage.append("CDM Version: ").append(cdmVersion.getSelectedItem());
+		cdmVersion = new JComboBox<>(new String[] { "cdm54" });
+		cdmVersion.setRenderer(new DefaultListCellRenderer() {
+			@Override
+			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+				super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				if ("cdm54".equals(value)) {
+					setText("CDM 5.4");
+				}
+				return this;
+			}
+		});
 
-        int result = JOptionPane.showConfirmDialog(this, confirmationMessage.toString(), "Continue or Cancel", JOptionPane.OK_CANCEL_OPTION);
-        if (result == JOptionPane.OK_OPTION) {
-            System.out.println("User chose to continue with the following selections:");
-            if (burnEverythingToTheGround.isSelected()) {
-            	System.out.println("- Burn Everything to the Ground");
-            }
-            if (createDatabase.isSelected()) {
-            	System.out.println("- Create Database");
-            }
-            if (createDatabaseUsers.isSelected()) {
-            	System.out.println("- Create Database Users");
-            }
-            if (createTables.isSelected()) {
-            	System.out.println("- Create Tables");
-            }
-            if (createCDMSourceRecord.isSelected()) {
-            	System.out.println("- Create CDM Source Record");
-            }
-            if (loadTerminology.isSelected()) {
-            	System.out.println("- Load Terminology");
-            }
-            if (createSequencesForPrimaryKeys.isSelected()) {
-            	System.out.println("- Create Sequences for Primary Keys");
-            }
-            if (createIndexes.isSelected()) {
-            	System.out.println("- Create Indexes");
-            }
-            if (addConstraints.isSelected()) {
-            	System.out.println("- Add Constraints");
-            }
-            System.out.println("Database type selected: " + databaseType.getSelectedItem());
-            System.out.println("CDM version selected: " + cdmVersion.getSelectedItem());
-        } else {
-            System.out.println("User canceled the action.");
-        }
-    }
+		controlPanel.add(databaseType);
+		controlPanel.add(cdmVersion);
 
-    public static void main(String[] args) {
-        new PolitesGui();
-    }
+		goButton = new JButton("Go");
+		goButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				handleGoButton();
+			}
+		});
+		controlPanel.add(goButton);
+
+		//
+		// final layout
+		//
+
+		add(scrollPane, BorderLayout.CENTER);
+		add(controlPanel, BorderLayout.SOUTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(650, 325);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	//
+	// handler for go button
+	//
+
+	private void handleGoButton() {
+		// Build the confirmation message
+		StringBuilder confirmationMessage = new StringBuilder("You have selected:\n");
+		if (burnEverythingToTheGround.isSelected())
+			confirmationMessage.append("- Burn Everything to the Ground\n");
+		if (createDatabase.isSelected())
+			confirmationMessage.append("- Create Database\n");
+		if (createDatabaseUsers.isSelected())
+			confirmationMessage.append("- Create Database Users\n");
+		if (createTables.isSelected())
+			confirmationMessage.append("- Create Tables\n");
+		if (createCDMSourceRecord.isSelected())
+			confirmationMessage.append("- Create CDM Source Record\n");
+		if (loadTerminology.isSelected())
+			confirmationMessage.append("- Load Terminology\n");
+		if (createSequencesForPrimaryKeys.isSelected())
+			confirmationMessage.append("- Create Sequences for Primary Keys\n");
+		if (createIndexes.isSelected())
+			confirmationMessage.append("- Create Indexes\n");
+		if (addConstraints.isSelected())
+			confirmationMessage.append("- Add Constraints\n");
+
+		confirmationMessage.append("Database Type: ").append(databaseType.getSelectedItem()).append("\n");
+		confirmationMessage.append("CDM Version: ").append(cdmVersion.getSelectedItem());
+
+		int result = JOptionPane.showConfirmDialog(this, confirmationMessage.toString(), "Continue or Cancel", JOptionPane.OK_CANCEL_OPTION);
+		if (result == JOptionPane.OK_OPTION) {
+			ArrayList<String> sel = new ArrayList<String>();
+			log.info("User chose to continue with the following selections:");
+			if (burnEverythingToTheGround.isSelected()) {
+				log.info("- Burn Everything to the Ground");
+				sel.add("burnEverythingToTheGround");
+			}
+			if (createDatabase.isSelected()) {
+				log.info("- Create Database");
+				sel.add("createDatabase");
+			}
+			if (createDatabaseUsers.isSelected()) {
+				log.info("- Create Database Users");
+				sel.add("createDatabaseUsers");
+			}
+			if (createTables.isSelected()) {
+				log.info("- Create Tables");
+				sel.add("createTables");
+			}
+			if (createCDMSourceRecord.isSelected()) {
+				log.info("- Create CDM Source Record");
+				sel.add("createCDMSourceRecord");
+			}
+			if (loadTerminology.isSelected()) {
+				log.info("- Load Terminology");
+				sel.add("loadTerminology");
+			}
+			if (createSequencesForPrimaryKeys.isSelected()) {
+				log.info("- Create Sequences for Primary Keys");
+				sel.add("createSequencesForPrimaryKeys");
+			}
+			if (createIndexes.isSelected()) {
+				log.info("- Create Indexes");
+				sel.add("createIndexes");
+			}
+			if (addConstraints.isSelected()) {
+				log.info("- Add Constraints");
+				sel.add("addConstraints");
+			}
+			log.info("Database type selected: " + databaseType.getSelectedItem());
+			log.info("CDM version selected: " + cdmVersion.getSelectedItem());
+			log.info("----------------------------------");
+			log.info("CALLING ACTION...");
+			ExecutePolitesGoAction.exec(sel,databaseType.getSelectedItem()+"",cdmVersion.getSelectedItem()+"");
+			log.info("DONE WITH ACTION.");
+			log.info("----------------------------------");
+			log.info("Done.");
+		} else {
+			log.info("User canceled the action.");
+		}
+	}
+
+	public static void main(String[] args) {
+		new PolitesGui();
+	}
 }
