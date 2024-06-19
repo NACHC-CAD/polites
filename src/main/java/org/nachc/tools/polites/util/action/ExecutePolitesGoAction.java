2b3ea792922f9b5ec6f8f23947b0b7068e076f12
greshje <greshje@gmail.com>
2024-06-18 21:43:42 -0400

    moving parameters

--- src/main/java/org/nachc/tools/polites/util/action/ExecutePolitesGoAction.java
+++ src/main/java/org/nachc/tools/polites/util/action/ExecutePolitesGoAction.java
@@ -18,6 +18,7 @@ import org.nachc.tools.fhirtoomop.tools.build.impl.EnableConstraints;
 import org.nachc.tools.fhirtoomop.tools.build.impl.LoadMappingTables;
 import org.nachc.tools.fhirtoomop.tools.build.impl.LoadTerminology;
 import org.nachc.tools.fhirtoomop.tools.build.impl.MoveRaceEthFiles;
+import org.nachc.tools.fhirtoomop.util.params.AppParams;
 import org.nachc.tools.polites.util.connection.PolitesConnectionFactory;
 import org.yaorma.database.Database;
 
@@ -48,6 +49,7 @@ public class ExecutePolitesGoAction {
 			if (sel.contains("createTables")) {
 				// create the tables
 				log("CREATING TABLES");
+				use(conn);
 				CreateDatabaseTables.exec(conn);
 				CreateFhirResoureTables.exec(conn);
 				CreateMappingTables.exec(conn);
@@ -55,31 +57,36 @@ public class ExecutePolitesGoAction {
 			if (sel.contains("createCDMSourceRecord")) {
 				// create the cdm_source record (uses app.parameters values)
 				log("CREATING CDM RECORD");
+				use(conn);
 				CreateCdmSourceRecord.exec(conn);
 				Database.commit(conn);
 			}
 			if (sel.contains("loadTerminology")) {
+				// load the terminologies
+				log("LOADING TERMINOLOGY");
 				// move the race eth files
 				MoveRaceEthFiles raceFiles = new MoveRaceEthFiles();
 				raceFiles.exec();
-				// load the terminologies
-				log("LOADING TERMINOLOGY");
+				use(conn);
 				LoadMappingTables.exec(raceFiles.getSqlFile(), conn);
 				LoadTerminology.exec(conn);
 			}
 			if (sel.contains("createSequencesForPrimaryKeys")) {
 				// create the sequences
 				log("CREATING SEQUENCES");
+				use(conn);
 				CreateSequencesForPrimaryKeys.exec(conn);
 			}
 			if (sel.contains("createIndexes")) {
 				// create the indexes and add constraints
 				log("CREATING CONSTRAINTS");
+				use(conn);
 				CreateDatabaseIndexes.exec(conn);
 			}
 			if (sel.contains("addConstraints")) {
 				// create the indexes and add constraints
 				log("ADDING CONSTRAINTS");
+				use(conn);
 				AddConstraints.exec();
 			}
 
@@ -87,22 +94,24 @@ public class ExecutePolitesGoAction {
 			if (sel.contains("disableConstraints")) {
 				// create the indexes and add constraints
 				log("DISABLING CONSTRAINTS");
+				use(conn);
 				DisableConstraints.exec(conn);
 			}
 			if (sel.contains("enableConstraints")) {
 				// create the indexes and add constraints
 				log("ENABLING CONSTRAINTS");
+				use(conn);
 				EnableConstraints.exec(conn);
 			}
 			if (sel.contains("uploadSyntheaCsv")) {
 				// create the indexes and add constraints
 				log("UPLOADING SYNTHEA CSV");
-
+				use(conn);
 			}
 			if (sel.contains("runAchilles")) {
 				// create the indexes and add constraints
 				log("RUNNING ACHILLES");
-
+				use(conn);
 			}
 		
 		
@@ -119,6 +128,13 @@ public class ExecutePolitesGoAction {
 		str += "* \n";
 		str += "* * * \n\n";
 		log.info(str);
+	}
+	
+	private static void use(Connection conn) {
+		log.info("Setting default schema...");
+		String schemaName = AppParams.getSchemaName();
+		Database.update("use " + schemaName, conn);		
+		log.info("Using: " + schemaName);
 	}
 	
 }
