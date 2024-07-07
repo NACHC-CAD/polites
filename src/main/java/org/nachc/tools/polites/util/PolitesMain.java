0f5c7db9c86c86528e580d9a355fa8c25689e2db
greshje <greshje@gmail.com>
2024-07-07 12:46:57 -0400

    work in progress on build

--- src/main/java/org/nachc/tools/polites/util/PolitesMain.java
+++ src/main/java/org/nachc/tools/polites/util/PolitesMain.java
@@ -1,10 +1,12 @@
 package org.nachc.tools.polites.util;
 
+import org.nachc.tools.fhirtoomop.util.params.AppParams;
 import org.nachc.tools.polites.util.gui.PolitesGui;
 
 public class PolitesMain {
 
 	public static void main(String[] args) {
+		AppParams.touch();
 		new PolitesGui();
 	}
 
